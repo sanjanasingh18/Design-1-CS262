7e0750b678c4a149784cf356c4d12b4c3a20ad93
Sanjana Singh <sanjanasingh@college.harvard.edu>
2023-02-02 21:04:59 -0500

    update ip

--- run_server.py
+++ run_server.py
@@ -70,7 +70,7 @@ class MySocket:
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 80))
+serversocket.bind(('10.250.7.238', 80))
 # become a server socket
 serversocket.listen(5)
 
