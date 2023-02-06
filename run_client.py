8f4c006e9b2d5d1f5779c47573ed0e68e4552b6f
Sanjana Singh <sanjanasingh@college.harvard.edu>
2023-02-06 17:27:24 -0500

    fix a little of login

--- run_client.py
+++ run_client.py
@@ -5,13 +5,23 @@ import math
 import time
 import uuid
 
+
+#[uuid: account info ]
+
+#account info is an object
+#recipients: queue of undelivered messages, logged in or not 
+#login TODO- if you login and you hvave undelivered messages, want to send those
+
 #this source code from https://docs.python.org/3/howto/sockets.html
 
 class ClientSocket:
-  # add an is_logged_in object for the account
-  is_logged_in = False
 
   def __init__(self, client=None):
+    # add an is_logged_in object for the account
+    self.logged_in = False
+
+    self.username = ''
+
     if client is None:
       self.client = socket.socket(
                         socket.AF_INET, socket.SOCK_STREAM)
@@ -22,8 +32,10 @@ class ClientSocket:
   def create_client_username(self, message, host, port):
     self.client.sendto(message.encode(), (host, port))
     data = self.client.recv(1024).decode()
-
-    print(data)
+    # update object attributes
+    self.username = data
+    self.logged_in = True
+    print('Your unique username is '  + data)
 
   #helper function to login to a client account
   def login_client_account(self, message, host, port):
@@ -48,12 +60,13 @@ class ClientSocket:
       #print("Unsuccessfully logged in")
       message = input("""We were unable to find an account associated with that username.
       Please type either 'create' to create a new account,
-      'exit' to close the server connection, 
+      'exit' to close the server connection/log out, 
       or re-enter your username.
       """)
       #exit- close the connection
       if message.lower().strip() == 'exit':
         print(f'Connection closed.')
+        self.logged_in = False
         self.client.close()
         break
       elif message.lower().strip() == 'create':
@@ -62,12 +75,20 @@ class ClientSocket:
       else: 
         #requery the client to see if this was a successful username
         #send over the username to the client
+        #prompt the server to look again for login
+        inform_status = 'login'
+        self.client.sendto(inform_status.encode(), (host, port))
+
         self.client.sendto(message.encode(), (host, port))
         
         #will receive back confrmation that you logged in successfully
         data = self.client.recv(1024).decode()
-
-        #print("Successfully logged in")
+    
+    # can exit while loop on success (logged in) or on a break 
+    if data == 'Account has been identified. Thank you!':
+      print("Successfully logged in.")
+      self.logged_in = True
+    
         #if data == 'Account has been identified. Thank you!':
         #  #successfully logged in
         #  print("Successfully logged in.")
@@ -84,8 +105,8 @@ class ClientSocket:
     data = self.client.recv(1024).decode()
 
     if data == 'Account successfully deleted.':
-      is_logged_in = False
-      print("Successfully deleted account and logged out.")
+      self.logged_in = False
+      print("Successfully deleted account.")
     else:
       print("Unsuccessfully deleted account.")
 
@@ -101,37 +122,36 @@ class ClientSocket:
       #handle initial information flow- either will login or create a new account
       
       #You need to either log in or create first
-      have_logged_in = False
-      while not have_logged_in:
+      while not self.logged_in:
         #handle initial information flow- either will login or create a new account
         message = input("""
         Welcome!
         Type 'login' to log into your account.
         Type 'create' to create a new account.
-        Type 'exit' to disconnect from server.
+        Type 'exit' to disconnect from server/log out.
         """)
 
         #login function
         if message.lower().strip() == 'login':
           self.login_client_account(message, host, port)
-          have_logged_in = True
+          break
 
         #create function
         elif message.lower().strip() == 'create':
           print("create_client")
           self.create_client_username(message, host, port)
-          have_logged_in = True
         
         #exit function- may want to exit early
         elif message.lower().strip() == 'exit':
           print(f'Connection closed.')
           self.client.close()
+          self.logged_in = False
           break
         
         #if it is none of these key words, it will re query until you enter 'login' or 'create'
 
       #you can only access this loop if you have logged in- if you aren't logged in then you exited
-      if have_logged_in:
+      if self.logged_in:
         #now, allow the client + server to interact until it says to exit
         message = input('Reply to server: ')
         
