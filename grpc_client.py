953c7c6ff72e88695ba56091cb82f4a0ae872643
Sanjana Singh <sanjanasingh@college.harvard.edu>
2023-02-18 11:09:40 -0500

    update password restrictions 1-950 chars

--- grpc_client.py
+++ grpc_client.py
@@ -117,8 +117,8 @@ class ClientSocket:
     pwd_client = input('Enter password: ')
 
     # What if the users do not enter a password? or one that is too long?
-    while len(pwd_client) < 1 or len(pwd_client) > 1000:
-      print("Error: password is required to be between 1 and 1000 characters")
+    while len(pwd_client) < 1 or len(pwd_client) > 950:
+      print("Error: password is required to be between 1 and 950 characters")
       pwd_client = input('Enter password: ')
 
 
